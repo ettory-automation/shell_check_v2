9788c5dfbb36d198ea5c33e6b53bb60b82c29581
Ettory Martins de Souza <ettory.devops@gmail.com>
2026-01-16 15:37:02 -0300

    Update banner title, font, and color

--- src/shell_check/core/banner.py
+++ src/shell_check/core/banner.py
@@ -2,12 +2,12 @@ import pyfiglet
 from rich.console import Console
 from rich.text import Text
 
-TITLE = 'AUDIT AUDIT ORCHESTRATOR'
-FONT = 'slant'
-COLOR = 'purple'
+TITLE = 'Shell Orchestrator'
+FONT = 'calvin_s'
+COLOR = 'cyan'
 
 console = Console()
 
 def banner():
-    ASCIITEXT = (pyfiglet.figlet_format(TITLE, font=FONT))
+    ASCIITEXT = (pyfiglet.figlet_format(TITLE.upper(), font=FONT))
     console.print(Text(ASCIITEXT, style=COLOR))
