8341c90edc7ca43bb93fa712fc73b326559d820f
Ettory Martins de Souza <ettory.devops@gmail.com>
2026-01-16 15:38:38 -0300

    Update banner title and font style

--- src/shell_check/ssh_runner/hosts_ctrl/banner_hostlist.py
+++ src/shell_check/ssh_runner/hosts_ctrl/banner_hostlist.py
@@ -2,12 +2,12 @@ import pyfiglet
 from rich.console import Console
 from rich.text import Text
 
-TITLE = 'SET HOSTLIST'
-FONT = 'slant'
+TITLE = 'Set Host Target'
+FONT = 'calvin_s'
 COLOR = 'purple'
 
 console = Console()
 
 def banner():
-    ASCIITEXT = (pyfiglet.figlet_format(TITLE, font=FONT))
+    ASCIITEXT = (pyfiglet.figlet_format(TITLE.upper(), font=FONT))
     console.print(Text(ASCIITEXT, style=COLOR))
